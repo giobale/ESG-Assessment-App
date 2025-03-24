103be9b1df75ab903cc321902167200a44554c7d
giobale <gbalestrieri.contact@gmail.com>
2025-03-25 00:27:14 +0100

    fix(dbt): log for root element

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+
+document.addEventListener('DOMContentLoaded', () => {
+    const rootElem = document.getElementById('root');
+    if (rootElem) {
+      createRoot(rootElem).render(<App />);
+    } else {
+      console.error('Root element not found');
+    }
+  });
